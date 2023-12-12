57bab13a414d49800d3ed87ccd6b9d7bc9212557
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2023-12-12 16:44:01 +0300

    add card component

---
+++ src/components/CardList.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Card from "./Card.vue";
+const onClickAdd = () => {
+	alert("add");
+};
+</script>
+<template>
+	<div class="grid grid-cols-4 gap-5">
+		<Card
+			title="Мужские кроссовки Nike Blazer Mid Suede"
+			imageUrl="/sneakers/sneakers-1.jpg"
+			:price="13000"
+			:onClickAdd="onClickAdd"
+		/>
+	</div>
+</template>
